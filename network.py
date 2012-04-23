671e286a2a4971fa32c53d6e1328f361fa30288e
Loris Fichera <loris.fichera@gmail.com>
2012-04-23 16:29:15 +0200

    Added spatial pooler for output node + learning algorithm for layer

--- network.py
+++ network.py
@@ -9,16 +9,30 @@ OUTPUT = 2
 class Network(object):
     def __init__(self, *args, **kwargs):
         self.layers = []
+        
+    def train(self):
+        pass
 
 
 class Layer(object):
     def __init__(self, *args, **kwargs):
+        self.nodes = []
+        self.spatial_pooler = None
         self.sigma = None
         self.distance_thr = None 
         self.node_sharing = False
         self.transition_memory_size = None
         self.group_max_size = None
         self.group_min_size = None
+        
+    def train(self, uClass, uTemporalGap=False):
+        if self.node_sharing:  ## train just the first node
+            spatial_pooler.train_node(self.nodes[0][0], uClass, uTemporalGap)
+            
+        else: ## train all nodes in the layer
+            for i in range(self.nodes):
+                for j in range(self.nodes[i]):
+                    spatial_pooler.train_node(self.nodes[i][j], uClass, uTemporalGap)
 
 
 class Node(object):
@@ -27,12 +41,18 @@ class Node(object):
         self.input_msg = np.array([])
         self.seen = np.array([])
         self.TAM = np.array([[]])
+        self.PCG = np.arry([[]])
         self.k = None
         self.k_prev = []
-    
-    
-class EntryNode(object):
-    pass
+        
+
+class OutputNode(object):
+    def __init__(self, *args, **kwargs):
+        self.coincidences = np.array([[]])
+        self.input_msg = np.array([])
+        self.k = None
+        self.cls_prior_prob = np.array([])
+        self.PCW = np.array([[]])
 
 
 class HTMBuilder(object):
--- spatial_clustering.py
+++ spatial_clustering.py
@@ -15,11 +15,11 @@ def widx_distance(diff):
 
 class SpatialPooler(object):
     """Implements the algorithms for clustering coincidences."""
-    def __init__(self, uThreshold, uTransitionMemory, *args, **kwargs):
+    def __init__(self, uThreshold, uTransitionMemory=0, *args, **kwargs):
         self.threshold = uThreshold
         self.transition_memory = uTransitionMemory
     
-    def train_node(self, uNode, uTemporalGap=False):
+    def train_node(self, uNode, uClass=0, uTemporalGap=False):
         """Train a node on the current input."""
         ## select the active threshold
         self.select_active_coinc(uNode)
@@ -109,17 +109,95 @@ class IntermediateSpatialPooler(SpatialPooler):
 
 
 class OutputSpatialPooler(SpatialPooler):
-    def train_node(self, uNode, uTemporalGap=False):
+    def train_node(self, uNode, uClass, uTemporalGap=False):
         """Train a node on the current input."""
         ## select the active threshold
         self.select_active_coinc(uNode)
         
-        ## !FIXME update the PCW matrix
+        ## update the PCW matrix
+        try: uNode.PCW[uNode.k, uClass] += 1
+        except:
+            (rows, cols) = uNode.PCW.shape
+            (delta_r, delta_c) = (uNode.k + 1 - rows, uClass + 1 - cols)
+            if delta_r < 0: delta_r = 0
+            if delta_c < 0: delta_c = 0
+            
+            uNode.PCW.resize((rows + delta_r, cols + delta_c), refcheck=False)
+            uNode.PCW[uNode.k, uClass] = 1
+        
 
+    def select_active_coinc(self, uNode):
+        """Given a node, selects its current active coincidence."""
+        ## if processing the first input pattern,
+        ## immediately make a new coincidence and return
+        if uNode.coincidences.size == 0:
+            uNode.coincidences = np.array([compute_widx(uNode.input_msg)])
+            uNode.k = 0
+            
+        else:
+            ## compute the distance of each coincidence from the
+            ## given input
+            w =compute_widx(uNode.input_msg)
+            distances = np.apply_along_axis(widx_distance, 1,
+                                            (uNode.coincidences - w))
+            uNode.k = np.argmin(distances)
+            minimum = distances[uNode.k]
+            
+            ## if the closest coincidence is not close enough,
+            ## make a new coincidence
+            if minimum > self.threshold:
+                uNode.coincidences = np.vstack((uNode.coincidences, w))
+                (uNode.k, _) = uNode.coincidences.shape
+                uNode.k -= 1
+        
 
 if __name__ == "__main__":
     # print widx_distance(np.array([1,2,3,4]), np.array([0,0,0,0]))
     # print widx_distance(np.array([1,2,3,4]), np.array([1,2,3,4]))
+
+    ## train output node
+    n = network.OutputNode()
+    s = OutputSpatialPooler(0)
+
+    n.input_msg = [ np.array([1,2,3,4]),
+                    np.array([4,5,6]),
+                    np.array([2,3,0.1]) ]
+    
+    s.train_node(n, 0)
+
+    print n.coincidences
+    print n.k
+    print n.PCW
+    print
+
+    s.train_node(n, 1)
+
+    print n.coincidences
+    print n.k
+    print n.PCW
+    print
+
+    n.input_msg = [ np.array([4,3,2,1]),
+                    np.array([6,5,4]),
+                    np.array([3,2,0.1]) ]
+
+    s.train_node(n, 1)
+
+    print n.coincidences
+    print n.k
+    print n.PCW
+    print
+
+    n.input_msg = [ np.array([0,0,0,0]),
+                    np.array([1,5,4]),
+                    np.array([3,10,0.1]) ]
+
+    s.train_node(n, 5)
+
+    print n.coincidences
+    print n.k
+    print n.PCW
+    print
     
     ## train intermediate node
     # n = network.Node()
