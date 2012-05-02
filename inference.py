52d0a3251d93b6a8b4b4f937afc97687feb97029
Loris Fichera <loris.fichera@gmail.com>
2012-05-02 11:55:56 +0200

    inference.py refactored

--- inference.py
+++ inference.py
@@ -1,163 +1,103 @@
-## the inference module
-
-#import network
+## global import ---------------------------------------------------------------
 import numpy as np
+
+
+## local import ----------------------------------------------------------------
 import utils
 
-def inference(uNode, uLayer, uSigma=0, uClass=None):
-    if uLayer == network.ENTRY:
-        p = EntryInferenceMaker()
-        return p.inference(uNode, uSigma)
 
-    elif uLayer == network.INTERMEDIATE:
-        p = IntermediateInferenceMaker()
-        return p.inference(uNode)
-    
-    else:
-        p = OutputInferenceMaker()
-        return p.inference(uNode)
-
-
+## -----------------------------------------------------------------------------
+## InferenceMaker Class
+## -----------------------------------------------------------------------------
 class InferenceMaker(object):
-    def dens_over_coinc(self, uCoincidences, uCurrentInput): pass
+    """Abstract class representing an inference maker."""
+    def dens_over_coinc(self, uCoincidences, uCurrentInput, uSigma=None):
+        (rows, cols) = uCoincidences.shape
+        y = np.array([[]])
+
+        for i in range(rows):
+            selected_coincidences = np.array([])
+
+            for j in range(cols):
+                selected_coincidences = \
+                    np.append(selected_coincidences, 
+                              uCurrentInput[j][0][uCoincidences[i,j]])
+
+            y = np.append(y, np.prod(selected_coincidences))
+            
+        return np.array([y])    
     
-    def dens_over_groups(self, uY, uPCG):
-        return np.dot(uY, uPCG)
+    def dens_over_matrix(self, uY, uMatrix):
+        return np.dot(uY, uMatrix)
+    
+    def inference(self, uNodeState): pass
 
-    def inference(self, uNode, uSigma=None):
-        y = utils.normalize_over_rows(
-            self.dens_over_coinc(uNode.coincidences,
-                                 uNode.input_msg,
-                                 uSigma))
 
-        z = self.dens_over_groups(y, uNode.PCG)
-        return z
-        
-
+## -----------------------------------------------------------------------------
+## EntryInferenceMaker Class
+## -----------------------------------------------------------------------------
 class EntryInferenceMaker(InferenceMaker):
+    """Implements inference algorithms for entry layer nodes."""
     def dens_over_coinc(self, uCoincidences, uCurrentInput, uSigma=1):
         y = np.sum(np.abs(uCoincidences - uCurrentInput)**2,axis=-1)**(1./2)
-        #y = np.apply_along_axis(np.linalg.norm, 1, (uCoincidences - uCurrentInput))
         y = np.exp(- np.power(y, 2) / np.power(uSigma, 2))
-        return np.array([y])
+        return y
+
+    def inference(self, uNodeState):
+        coinc = uNodeState['coincidences']
+        input_msg = uNodeState['input_msg']
+        sigma = uNodeState['sigma']
+        PCG = uNodeState['PCG']
+        
+        y = self.dens_over_coinc(coinc, input_msg, sigma)
+        y = utils.normalize_over_rows(y)
+        z = self.dens_over_matrix(y, PCG)
+        
+        uNodeState['output_msg'] = z
 
 
+## -----------------------------------------------------------------------------
+## EntryInferenceMaker Class
+## -----------------------------------------------------------------------------
 class IntermediateInferenceMaker(InferenceMaker):
-    def dens_over_coinc(self, uCoincidences, uCurrentInput, uSigma=None):
-        (rows, cols) = uCoincidences.shape
-        y = np.array([[]])
+    """Implements inference algorithms for intermediate layer nodes."""
+    def inference(self, uNodeState):
+        coinc = uNodeState['coincidences']
+        input_msg = uNodeState['input_msg']
+        PCG = uNodeState['PCG']
+        
+        y = self.dens_over_coinc(coinc, input_msg, sigma)
+        y = utils.normalize_over_rows(y)
+        z = self.dens_over_matrix(y, PCG)
 
-        for i in range(rows):
-            selected_coincidences = np.array([])
+        uNodeState['output_msg'] = z
 
-            for j in range(cols):
-                selected_coincidences = \
-                    np.append(selected_coincidences, 
-                              uCurrentInput[j][0][uCoincidences[i,j]])
 
-            y = np.append(y, np.prod(selected_coincidences))
-            
-        return np.array([y])
-    
-
+## -----------------------------------------------------------------------------
+## OutputInferenceMaker Class
+## -----------------------------------------------------------------------------
 class OutputInferenceMaker(InferenceMaker):
-    def dens_over_coinc(self, uCoincidences, uCurrentInput, uSigma=None):
-        (rows, cols) = uCoincidences.shape
-        y = np.array([[]])
-
-        for i in range(rows):
-            selected_coincidences = np.array([])
-
-            for j in range(cols):
-                selected_coincidences = \
-                    np.append(selected_coincidences, 
-                              uCurrentInput[j][0][uCoincidences[i,j]])
-
-            y = np.append(y, np.prod(selected_coincidences))
-            
-        return np.array([y])
-
-    def dens_over_classes(self, uY, uPCW):
-        return np.dot(uY, uPCW)
-    
+    """Implements inference algorithms for output nodes."""
     def class_post_prob(self, uZ, uClassPriorProb):
         total = np.dot(uZ, uClassPriorProb)
         return uZ * uClassPriorProb / total
     
-    def inference(self, uNode):
-        y = utils.normalize_over_rows(
-            self.dens_over_coinc(uNode.coincidences,
-                                 uNode.input_msg))
-        z = self.dens_over_classes(y, uNode.PCW)
-        p = self.class_post_prob(z, uNode.cls_prior_prob)
+    def inference(self, uNodeState):
+        coinc = uNodeState['coincidences']
+        input_msg = uNodeState['input_msg']
+        cls_prior_prob = uNodeState['cls_prior_prob']
+        PCW = uNodeState['PCW']
         
-        return p
+        y = self.dens_over_coinc(coinc, input_msg, sigma)
+        y = utils.normalize_over_rows(y)
+        z = self.dens_over_matrix(y, PCW)
+        p = self.class_post_prob(z, cls_prior_prob)
+
+        uNodeState['output_msg'] = p
 
 
 if __name__ == "__main__":
     from network import Node
     from spatial_clustering import EntrySpatialPooler
-    
-    n = Node()
 
-    ## train entry node
-    n = network.Node()
-    s = EntrySpatialPooler(0, 5)
-    n.input_msg = np.array([1,2,3,4])
-    s.train_node(n)
-    print n.coincidences
-    print n.seen
-    print n.TAM
-    print n.k
-    print n.k_prev
-    print
-
-    s.train_node(n)
-
-    print n.coincidences
-    print n.seen
-    print n.TAM
-    print n.k
-    print n.k_prev
-    print
-
-    n.input_msg = np.array([0,0,0,0])
-    s.train_node(n)
-
-    print n.coincidences
-    print n.seen
-    print n.TAM
-    print n.k
-    print n.k_prev
-    print
-
-    n.input_msg = np.array([1,1,1,1])
-    s.train_node(n)
-    
-    print n.coincidences
-    print n.seen
-    print n.TAM
-    print n.k
-    print n.k_prev
-    print
-
-    n.input_msg = np.array([1,2,3,4])
-    s.train_node(n)
-    
-    print n.coincidences
-    print n.seen
-    print n.TAM
-    print n.k
-    print n.k_prev
-    print
-
-    print "Now making inference"
-    e = EntryInferenceMaker()
-    e.inference(n, 25.0)
-
-    print n.output_msg
-    print n.coincidences
-    print n.seen
-    print n.TAM
-    print n.k
-    print n.k_prev
+    pass
