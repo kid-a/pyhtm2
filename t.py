7108a6fdc5c695d474ed7c1ae0441e45af2777c1
Loris Fichera <loris.fichera@gmail.com>
2012-05-04 09:56:28 +0200

    test

--- t.py
+++ t.py
@@ -24,10 +24,9 @@ if __name__ == "__main__":
     htm = builder.build()
     htm.start()
     
-    
+    ## train layer 0
     for j in range(4):
         for i in range(10):
-            print i
             image = usps.read("data_sets/train100/" + str(j) + "/" + str(i+1) + ".bmp")
             htm.expose(image)
             if i == 0: htm.layers[0].train({'temporal_gap' : True})
@@ -35,20 +34,34 @@ if __name__ == "__main__":
     
     htm.layers[0].finalize()
 
-    for i in range(4):
-        for j in range(4):
-            htm.layers[0].nodes[i][j].input_channel.put("clone_state")
-            print i, j, htm.layers[0].nodes[i][j].output_channel.get()
+    ## train layer 1
+    for j in range(4):
+        for i in range(10):
+            print i
+            image = usps.read("data_sets/train100/" + str(j) + "/" + str(i+1) + ".bmp")
+            htm.expose(image)
+            htm.layers[0].inference()
+            htm.propagate(0, 1)
+            
+            if i == 0: htm.layers[1].train({'temporal_gap' : True})
+            else: htm.layers[1].train({'temporal_gap' : False})
+
+    for i in range(2):
+        for j in range(2):
+            htm.layers[1].nodes[i][j].input_channel.put("clone_state")
+            print i, j, htm.layers[1].nodes[i][j].output_channel.get()
             
 
-    for i in range(1):        
-        print i
-        image = usps.read("data_sets/train100/0/" + str(i+1) + ".bmp")
-        htm.expose(image)
-        htm.layers[0].inference()
-        htm.layers[0].nodes[0][0].input_channel.put("get_output")
-        print "out, first layer: ", htm.layers[0].nodes[0][0].output_channel.get()
-        htm.propagate(0, 1)
+    # for i in range(1):        
+    #     print i
+    #     image = usps.read("data_sets/train100/0/" + str(i+1) + ".bmp")
+    #     htm.expose(image)
+    #     htm.layers[0].inference()
+    #     htm.layers[0].nodes[0][0].input_channel.put("get_output")
+    #     print "out, first layer: ", htm.layers[0].nodes[0][0].output_channel.get()
+    #     htm.propagate(0, 1)
+
+
         
         
         
